8aa5d0d41f447664971e775aa43d83694ca1659a
Panqueque <108034714+Panquequ3@users.noreply.github.com>
2026-05-30 01:16:57 -0400

    conexión con postgres (modulo busqueda)

--- backend/mod_busqueda/service.py
+++ backend/mod_busqueda/service.py
@@ -1,7 +1,11 @@
+import os
 # Importamos Flask (para crear la API web) y jsonify (para responder en formato JSON)
 from flask import Flask, request, jsonify
 # Importamos pysolr (la librería para comunicarnos con Solr)
 import pysolr
+
+import psycopg2 
+from psycopg2.extras import RealDictCursor
 
 # 1. Inicializamos la aplicación Flask
 app = Flask(__name__)
@@ -10,6 +14,15 @@ app = Flask(__name__)
 # "solr_engine" es el nombre que le dimos al contenedor en el docker-compose.yml
 # "8983" es el puerto, y "catalogo" es el core que creamos.
 SOLR_URL = 'http://solr_engine:8983/solr/catalogo'
+
+# Función auxiliar para conectarnos a la BD
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.environ.get('DB_HOST', 'localhost'),
+        database='db_catalogo',
+        user=os.environ.get('POSTGRES_USER', 'admin'),
+        password=os.environ.get('POSTGRES_PASSWORD', 'adminpassword')
+    )
 
 # Creamos el "cliente" que hablará con Solr
 solr = pysolr.Solr(SOLR_URL, always_commit=True)
@@ -43,6 +56,32 @@ def buscar_juegos():
     except Exception as e:
         # Si algo falla (ej. Solr está apagado), devolvemos un error 500
         return jsonify({"error": "Fallo en el servidor de búsqueda", "detalle": str(e)}), 500
+
+# Nueva ruta que recibe el ID del juego en la URL (ej: /juego/splatoon-3-ext)
+@app.route('/juego/<id_juego>', methods=['GET'])
+def obtener_detalle_juego(id_juego):
+    conn = None
+    try:
+        conn = get_db_connection()
+        # RealDictCursor hace que la respuesta sea {"titulo": "x", "precio": y} en vez de una tupla rara ("x", y)
+        cur = conn.cursor(cursor_factory=RealDictCursor) 
+        
+        # Hacemos la consulta SQL a la tabla catalogo que definieron en su diagrama
+        cur.execute("SELECT * FROM catalogo WHERE id_juego = %s", (id_juego,))
+        juego = cur.fetchone() # fetchone() trae solo 1 resultado
+        
+        cur.close()
+        
+        if juego is None:
+            return jsonify({"error": "Juego no encontrado"}), 404
+            
+        return jsonify(juego), 200
+
+    except Exception as e:
+        return jsonify({"error": "Fallo en la base de datos", "detalle": str(e)}), 500
+    finally:
+        if conn is not None:
+            conn.close() 
 
 # 6. Encender el servidor
 if __name__ == '__main__':

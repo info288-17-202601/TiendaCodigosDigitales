39a930bd2a1366300b3bc830e406b15529bcdcd8
lucianouu <luciano.espinoza@alumnos.uach.cl>
2026-06-18 23:44:19 -0400

    línea front corregida

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useCart } from '../context/CartContext';
 import { api } from '../api';
-const correoPrueba = process.env.REACT_APP_CORREO_DESTINO;
+const correoPrueba = "correo.prueba@gmail.com";
 
 const Cart = ({ onNavigate }) => {
   const { cart, removeFromCart, clearCart, loading } = useCart();

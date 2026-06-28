ec0e7c6575fe805c007613b691014b27fc34134a
spartanpotato <107315314+spartanpotato@users.noreply.github.com>
2026-06-27 23:15:24 -0400

    incorporacion token en ventas, extension automatica ttl

--- backend/mod_ventas/api.py
+++ backend/mod_ventas/api.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from shared.database import get_connection, release_connection
-from shared.cache import get_carrito, set_carrito
+from shared.cache import get_carrito, set_carrito, get_sesion
 from mod_ventas.service import procesar_checkout
 
 FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://localhost:3000")
@@ -22,7 +22,23 @@ def obtener_carrito():
     """
     # Por ahora usamos un usuario_id por defecto
     # En un caso real, vendría del header o JWT
-    usuario_id = "user-123"
+    #usuario_id = "user-123"
+
+    data = request.get_json()
+
+    # Validacion manual de que traiga todos los datos
+    campos_requeridos = ["token_sesion"]
+    if not data or not all(campo in data for campo in campos_requeridos):
+        return jsonify({"error": "Faltan campos obligatorios en el JSON"}), 400
+    
+    sesion_usuario = get_sesion(data['token_sesion'])
+    
+    # Si no existe el registro
+    if sesion_usuario == None:
+        return jsonify({"error": "No existe sesion valida con el token dado"}), 401
+    
+    # si existe obtenemos el carrito y lo devolvemos
+    usuario_id = sesion_usuario['usuario_id']
     
     carrito = get_carrito(usuario_id)
     return jsonify(carrito), 200
@@ -36,13 +52,19 @@ def agregar_al_carrito():
     data = request.get_json()
     
     # Validacion manual de que traiga todos los datos
-    campos_requeridos = ["usuario_id", "juego_id", "cantidad", "precio_unitario", "titulo"]
+    campos_requeridos = ["token_sesion", "juego_id", "cantidad", "precio_unitario", "titulo"]
     if not data or not all(campo in data for campo in campos_requeridos):
         return jsonify({"error": "Faltan campos obligatorios en el JSON"}), 400
-
-    usuario_id = data['usuario_id']
     
-    # Prepar el nuevo item
+    sesion_usuario = get_sesion(data['token_sesion'])
+    
+    # Si no existe el registro
+    if sesion_usuario == None:
+        return jsonify({"error": "No existe sesion valida con el token dado"}), 401
+
+    usuario_id = sesion_usuario['usuario_id']
+    
+    # Preparar el nuevo item
     nuevo_item = {
         "juego_id": data['juego_id'],
         "cantidad": int(data['cantidad']),
@@ -66,7 +88,7 @@ def agregar_al_carrito():
     # Dar respuesta
     return jsonify({
         "mensaje": "Producto añadido al carrito exitosamente", 
-        "usuario_id": usuario_id,
+        "token_sesion": data['token'],
         "total_estimado": carrito_actual["total_estimado"]
     }), 200
 
@@ -79,12 +101,20 @@ def iniciar_checkout():
     data = request.get_json()
     
     # Validacion manual de que traiga el usuario
-    campos_requeridos = ["usuario_id", "email","metodo_pago"]
+    campos_requeridos = ["token_sesion", "email","metodo_pago"]
     if not data or not all(campo in data for campo in campos_requeridos):
         return jsonify({"error": "Faltan campos obligatorios en el JSON"}), 400
 
+    sesion_usuario = get_sesion(data['token_sesion'])
+    
+    # Si no existe el registro
+    if sesion_usuario == None:
+        return jsonify({"error": "No existe sesion valida con el token dado"}), 401
+
+    usuario_id = sesion_usuario['usuario_id']
+
     # Utilizar servicio del modulo de ventas
-    id_orden = procesar_checkout(data['usuario_id'], data['email'], data['metodo_pago'])
+    id_orden = procesar_checkout(usuario_id, data['email'], data['metodo_pago'])
     
     # Si ocurre un error en el servicio especificamente
     if not id_orden:
@@ -99,12 +129,27 @@ def iniciar_checkout():
     }), 202
 
 # Quitar un elemento especifico del carrito
-@app.route("/api/ventas/carrito/<usuario_id>/item/<juego_id>", methods=["DELETE"])
-def quitar_del_carrito(usuario_id, juego_id):
+@app.route("/api/ventas/carrito/item/<juego_id>", methods=["DELETE"])
+def quitar_del_carrito(juego_id):
     """
     Elimina solo la primera instancia de un juego especifico 
     del carrito temporal del usuario y recalcula el total.
     """
+
+    data = request.get_json()
+    # Validacion manual de que traiga todos los datos
+    campos_requeridos = ["token_sesion"]
+    if not data or not all(campo in data for campo in campos_requeridos):
+        return jsonify({"error": "Faltan campos obligatorios en el JSON"}), 400
+    
+    sesion_usuario = get_sesion(data["token_sesion"])
+
+    # Si no existe el registro
+    if sesion_usuario is None:
+        return jsonify({"error": "No existe sesion valida con el token dado"}), 401
+
+    usuario_id = sesion_usuario["usuario_id"]
+
     # Obtener el carrito actual
     carrito_actual = get_carrito(usuario_id)
     
@@ -141,11 +186,26 @@ def quitar_del_carrito(usuario_id, juego_id):
     }), 200
 
 # Vaciar todo el carrito de un usuario
-@app.route("/api/ventas/carrito/<usuario_id>", methods=["DELETE"])
+@app.route("/api/ventas/carrito", methods=["DELETE"])
 def vaciar_carrito(usuario_id):
     """
     Limpia por completo el carrito de compras de un usuario.
     """
+
+    data = request.get_json()
+    # Validacion manual de que traiga todos los datos
+    campos_requeridos = ["token_sesion"]
+    if not data or not all(campo in data for campo in campos_requeridos):
+        return jsonify({"error": "Faltan campos obligatorios en el JSON"}), 400
+    
+    sesion_usuario = get_sesion(data["token_sesion"])
+
+    # Si no existe el registro
+    if sesion_usuario is None:
+        return jsonify({"error": "No existe sesion valida con el token dado"}), 401
+
+    usuario_id = sesion_usuario["usuario_id"]
+
     carrito_actual = get_carrito(usuario_id)
     
     # Reiniciar valores
@@ -158,7 +218,7 @@ def vaciar_carrito(usuario_id):
     # Dar respuesta
     return jsonify({
         "mensaje": "El carrito ha sido vaciado exitosamente",
-        "usuario_id": usuario_id,
+        "token_sesion": data['token_sesion'],
         "total_estimado": 0.0
     }), 200
 
@@ -168,6 +228,19 @@ def consultar_estado_orden(id_orden_compra):
     """
     Endpoint para Polling del estado de la orden por parte del frontend.
     """
+
+    data = request.get_json()
+    # Validacion manual de que traiga todos los datos
+    campos_requeridos = ["token_sesion"]
+    if not data or not all(campo in data for campo in campos_requeridos):
+        return jsonify({"error": "Faltan campos obligatorios en el JSON"}), 400
+    
+    sesion_usuario = get_sesion(data["token_sesion"])
+
+    # Si no existe el registro
+    if sesion_usuario is None:
+        return jsonify({"error": "No existe sesion valida con el token dado"}), 401
+
     db_name = "db_ventas"
 
     # Obtener conexion
@@ -194,11 +267,26 @@ def consultar_estado_orden(id_orden_compra):
         release_connection(db_name, conn)
 
 # Obtener historial de ordenes
-@app.route("/api/ventas/ordenes/usuario/<usuario_id>", methods=["GET"])
-def historial_compras_usuario(usuario_id):
+@app.route("/api/ventas/ordenes", methods=["GET"])
+def historial_compras_usuario():
     """
     Devuelve el historial inmutable de compras para un usuario.
     """
+
+    data = request.get_json()
+    # Validacion manual de que traiga todos los datos
+    campos_requeridos = ["token_sesion"]
+    if not data or not all(campo in data for campo in campos_requeridos):
+        return jsonify({"error": "Faltan campos obligatorios en el JSON"}), 400
+    
+    sesion_usuario = get_sesion(data["token_sesion"])
+
+    # Si no existe el registro
+    if sesion_usuario is None:
+        return jsonify({"error": "No existe sesion valida con el token dado"}), 401
+
+    usuario_id = sesion_usuario["usuario_id"]
+
     db_name = "db_ventas"
 
     # Obtener conexion
@@ -218,21 +306,13 @@ def historial_compras_usuario(usuario_id):
         
         # Dar respuesta
         return jsonify({
-            "usuario_id": usuario_id, 
+            "token_sesion": data['token_sesion'], 
             "historial": ordenes
         }), 200
         
     # Liberar conexion
     finally:
         release_connection(db_name, conn)
-
-# Obtener el carrito actual
-@app.route("/api/ventas/carrito/usuario/<usuario_id>", methods=["GET"])
-def carrito_usuario(usuario_id):
-    """
-    Devuelve el estado actual del carrito del usuario <usuario_id>
-    """
-    return jsonify(get_carrito(usuario_id), 200)
 
 
 if __name__ == "__main__":
